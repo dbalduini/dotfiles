b0935ed2aa7e62e059e4533422c80921adbe1311
Diego Balduini <balduini@br.ibm.com>
2016-07-20 20:09:35 -0300

    prompt for replace

--- dotfiles_updater.sh
+++ dotfiles_updater.sh
@@ -1,9 +1,21 @@
-#!/bin/zsh
+#!/usr/bin/env zsh 
+
+Replace () {
+  read -q "RESP?Do you want to replace file $1? [y/N]"
+  echo $RESP
+  if [[ "$RESP" == 'y' ]]
+  then
+    cp ~/$input .
+    echo "File replaced!"
+  else
+    echo "File NOT replaced!"
+  fi
+}
 
 
 while read input
 do
-  echo "\nChecking dotfile $input..."
+  echo "Checking dotfile $input..."
   if [ ! -f "$input" ]
   then
     echo "Copying new dotfile $input..."
@@ -18,7 +30,6 @@ do
   then
     echo "File $input has changed!"
     echo "Old: $old_sum | New: $new_sum"
-    cp ~/$input .
-    echo "Replace done!"
+    Replace $input 
   fi
 done <dotfiles.txt
