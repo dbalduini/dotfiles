64407ef2910d2b193222b245ba41f583b2055582
Diego Balduini <balduini@br.ibm.com>
2016-07-20 21:50:44 -0300

    ADd install support

--- dotfiles.sh
+++ dotfiles.sh
@@ -3,57 +3,85 @@
 # FLAGS
 FLAG_INSTALL=false
 FLAG_VERBOSE=false
+FLAG_FORCE=false
 
 # FUNCTIONS
 Replace () {
   read -q "RESP?Do you want to replace $1? [y/N]"
-  echo $RESP
   if [[ "$RESP" == 'y' ]]
   then
-    echo "Replacing file $1..."
     cp ~/$1 .
-    echo "File replaced!"
+    echo "\nFile replaced!"
   else
-    echo "File NOT replaced!"
+    echo "\nFile NOT replaced!"
   fi
 }
 
 # PROGRAM
 
 ## Read cmd args
-while getopts ":iv" opt
+while getopts ":ivf" opt
 do
   case $opt in
     i)
       FLAG_INSTALL=true;;
     v)
       FLAG_VERBOSE=true;;
+    f)
+      FLAG_FORCE=true;;
     \?)
       echo "Invalid option: -$OPTARG"
       ;;
   esac
 done
 
-## Run dotfiles
-while read input
-do
-  echo "Checking dotfile $input..."
+AddNew() {
+  echo
+  echo "Checking dotfile $input"
   if [ ! -f "$input" ]
   then
-    echo "Adding new dotfile $input..."
+    echo "Adding new dotfile $input"
     cp ~/$input .
     echo "File sucessfully added!"
-  fi 
+  fi
+}
 
+UpdateRepository() {
   old_sum=$(cat $input | md5)
   new_sum=$(cat ~/$input | md5)
 
-  if [ "$old_sum" != "$new_sum" ]
+  if $FLAG_FORCE || [ "$old_sum" != "$new_sum" ]
   then
-    echo "File $input has changed!"
+
     if $FLAG_VERBOSE; then
-      echo "Old: $old_sum | New: $new_sum"
+      if [ "$old_sum" != "$new_sum" ]; then
+        echo "Files have different checksum!"
+        echo ":::CHECKSUM:::"
+        echo "Old: $old_sum | New: $new_sum"
+        echo ""
+      else
+        echo "Both files are the same"
+      fi
     fi
     Replace $input
+  fi
+}
+
+## Run dotfiles
+while read input
+do
+
+  if $FLAG_INSTALL; then
+    if $FLAG_FORCE || [ ! -f ~/$input ]; then
+      echo "Installing ~/$input ..."
+      if cp $input ~/$input ; then
+        echo "~/$input was sucessfully installed!"
+      fi
+    else
+      echo "File already exists: ~/$input"
+    fi
+  else
+    AddNew
+    UpdateRepository
   fi
 done <dotfiles.txt
